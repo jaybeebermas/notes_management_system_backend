ae4dc3ce05337d947ebca87654b8134e76e74239
jabermas <bermasjaybee9@gmail.com>
2026-03-17 11:54:25 +0800

    added auth in api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\NoteController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['api'])->group(function (): void {
+Route::middleware(['api', 'auth:sanctum'])->group(function (): void {
     Route::get('notes', [NoteController::class, 'index']);
     Route::post('notes', [NoteController::class, 'store']);
     Route::get('notes/{id}', [NoteController::class, 'show']);
